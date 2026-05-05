1fbffa73aabf83694744e6848b591ad95714573e
elder-banshee <ronan.roue@gmail.com>
2026-05-05 04:25:06 +0200

    Clean up spacer styling and logging
    Removed red background and console logs from spacer insertion.

--- js/chroniques.js
+++ js/chroniques.js
@@ -81,11 +81,8 @@ function renderCartes() {
     spacer.style.pointerEvents = 'none';
     spacer.style.flexShrink = '0';
     spacer.style.flexBasis = '100%';
-    spacer.style.background = 'red';
     const outer = document.querySelector('.chrono-piste-outer');
     outer.insertAdjacentElement('afterend', spacer);
-    console.log('spacer inséré :', spacer);
-    console.log('parent du spacer :', spacer.parentNode);
     }
   });
 
